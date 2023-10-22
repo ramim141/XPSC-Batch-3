ff093a4885968f5df28aba244bdd225a2ff1f95a
Ramim Ahmed <125358464+ramim141@users.noreply.github.com>
2023-10-22 21:11:22 +0600

    Add files via upload

---
+++ Week 04/B_Even_Odd_Increments.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	long long t,n,q;
+	cin>>t;
+	
+	while(t--){
+		int x,odd=0,even=0;
+		cin>>n>>q;
+			long long ans=0;
+			for (int i = 0; i < n; i++) {
+			cin >> x;
+			if (x % 2 == 0)odd++;
+			else even++;
+			ans += x;
+		}
+		int p;
+		while(q--){
+				cin >> x>> p;
+			if (x == 0) { 
+				ans += p * odd;
+				if (p % 2 == 1) even += odd, odd = 0;
+			}
+			else { 
+				ans += p * even;
+				if (p % 2 == 1)odd += even, even = 0;
+			}
+			cout << ans << endl;
+			}
+		}
+	return 0;
+}
+  	 	 	 		  	 	 	 		  								
